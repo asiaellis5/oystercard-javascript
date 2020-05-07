308c029c19a242552e11d6993757917403f6c7b2
Asia <asiaellis5@gmail.com>
2020-05-07 12:09:01 +0100

    added error to topUpCard if maximum balance exceeded

--- jasmine-standalone-3.5.0/spec/oystercardSpec.js
+++ jasmine-standalone-3.5.0/spec/oystercardSpec.js
@@ -1,3 +1,5 @@
+"use strict";
+
 describe("OysterCard", function () {
   let oysterCard;
 
@@ -9,12 +11,46 @@ describe("OysterCard", function () {
     it("starts with a balance of 0", function () {
       expect(oysterCard.balance).toEqual(0);
     });
+
+    it("has a maximum balance of £90", function () {
+      expect(oysterCard.maximumBalance).toEqual(90);
+    });
   });
 
   describe("topUpCard", function () {
     it("tops up the balance on card", function () {
       oysterCard.topUpCard(10);
       expect(oysterCard.balance).toEqual(10);
+    });
+
+    it("returns a statement that confirms the money has been topped up", function () {
+      expect(oysterCard.topUpCard(10)).toEqual(
+        "£10 Sucessfully added, Balance = £10"
+      );
+    });
+
+    it("throws an error if maximum balance exceeded", function () {
+      for (let i = 0; i < 8; i++) {
+        oysterCard.topUpCard(10);
+      }
+      expect(function () {
+        oysterCard.topUpCard(11);
+      }).toThrowError("Maximum balance exceeded");
+    });
+  });
+
+  describe("deductFare", function () {
+    it("deducts the standard fare", function () {
+      oysterCard.topUpCard(10);
+      oysterCard.deductFare();
+      expect(oysterCard.balance).toEqual(7);
+    });
+
+    it("returns a statement that money has been deducted", function () {
+      oysterCard.topUpCard(10);
+      expect(oysterCard.deductFare()).toEqual(
+        "£3 deducted, current balance: £7"
+      );
     });
   });
 });
--- jasmine-standalone-3.5.0/src/oystercard.js
+++ jasmine-standalone-3.5.0/src/oystercard.js
@@ -1,9 +1,22 @@
+"use strict";
+
 class OysterCard {
   constructor() {
     this.balance = 0;
+    this.maximumBalance = 90;
   }
 
   topUpCard = (topUpAmount) => {
-    return (this.balance += topUpAmount);
+    if (this.balance + topUpAmount < this.maximumBalance) {
+      this.balance += topUpAmount;
+      return `£${topUpAmount} Sucessfully added, Balance = £${this.balance}`;
+    } else {
+      throw new Error("Maximum balance exceeded");
+    }
+  };
+
+  deductFare = () => {
+    this.balance -= 3;
+    return `£3 deducted, current balance: £${this.balance}`;
   };
 }
