48db2d8e40542535c5e934d36b5ba17d6d6e3321
Asia <asiaellis5@gmail.com>
2020-05-08 09:56:11 +0100

    another feature spec over different number of zones

--- jasmine-standalone-3.5.0/spec/featureSpec.js
+++ jasmine-standalone-3.5.0/spec/featureSpec.js
@@ -23,4 +23,13 @@ describe("feature", function () {
     );
     expect(oysterCard.balance).toEqual(5);
   });
+
+  it("touches in and out correctly and deducts the correct fare", function () {
+    oysterCard.topUpCard(20);
+    oysterCard.touchIn(stationOne);
+    oysterCard.touchOut(stationThree);
+    expect(oysterCard.journeyHistory.history[0].entryStation.zone).toEqual(1);
+    expect(oysterCard.journeyHistory.history[0].exitStation.zone).toEqual(6);
+    expect(oysterCard.balance).toEqual(12);
+  });
 });
